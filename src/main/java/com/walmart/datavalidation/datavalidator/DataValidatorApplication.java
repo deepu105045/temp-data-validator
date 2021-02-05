7015e14d506b6590b1a30b389a419665f2d519bc
deepu105045 <deepu105045@gmail.com>
2021-02-05 15:27:44 +0530

    draft

---
+++ src/main/java/com/walmart/datavalidation/datavalidator/DataValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.datavalidation.datavalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataValidatorApplication.class, args);
+	}
+
+}
